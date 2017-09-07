b341ac48af30e2b069d4558f4c392b2d368c8638
Jay van Schyndel <ozej8y@gmail.com>
2017-09-07 11:25:44 +1000

    DataFix for People harvesting to support ORCIDs

---
+++ src/main/config/home/dataFixes/upgradeTo19.sh
@@ -0,0 +1,12 @@
+# Copy the new Parties_People_TDH.json and .py to ensure the new settings are used when harvesting.
+# Changes were made to support ORCID harvesting
+cp ../harvest/Parties_People_TDH.json ../../storage/1793582ab247f6442162a75562dcc548/16/c8/ac/16c8ac40db19ac7cca3873771bb81126/
+cp ../harvest/Parties_People_TDH.json ../../storage/1793582ab247f6442162a75562dcc548/66/fd/f9/66fdf9ae9dc9478eeb05678f88cec58d/
+cp ../harvest/Parties_People_TDH.json ../../storage/1793582ab247f6442162a75562dcc548/a3/43/c9/a343c913e8f343ba0d1d39581f87d6b8/
+cp ../harvest/Parties_People_TDH.json ../../storage/1793582ab247f6442162a75562dcc548/c3/5e/41/c35e41ca9b3622cc8302206e78751f2b/
+
+
+cp ../harvest/Parties_People_TDH.py ../../storage/1793582ab247f6442162a75562dcc548/69/29/b3/6929b3f0646edb934e86d63621eca9a3/
+cp ../harvest/Parties_People_TDH.py ../../storage/1793582ab247f6442162a75562dcc548/bb/67/35/bb673587caa9502c64f5fab1299929dc/
+cp ../harvest/Parties_People_TDH.py ../../storage/1793582ab247f6442162a75562dcc548/c5/26/b3/c526b351ee465aaff55fd6a6523574fc/
+cp ../harvest/Parties_People_TDH.py ../../storage/1793582ab247f6442162a75562dcc548/e8/c9/36/e8c936134abaadb0d7f5a924d140a881/
